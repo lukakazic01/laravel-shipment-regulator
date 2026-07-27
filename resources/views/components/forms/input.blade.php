43576b3c200b1efcf88f6b58fb39c8450bb32338
lukakazic01 <kazicluka4@gmail.com>
2026-07-27 13:26:00 +0200

    Create textarea form component, minor modifications

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -4,14 +4,19 @@
 ])
 
 <input
-    type="text"
     name="{{ $name }}"
     id="{{ $name }}"
+    value="{{ $value }}"
     @if ($required) required @endif
     {{
-        $attributes->class([
-            'w-full bg-white border border-gray-200 text-secondary rounded px-4 py-2 outline-none transition-all duration-150 focus:shadow-md focus:shadow-primary/15 focus:ring-2 focus:ring-primary',
-            'border-red-500' => $errors->has('name'),
-        ])
+        $attributes
+            ->class([
+                'w-full bg-white border border-gray-200 text-secondary rounded px-4 py-2 outline-none transition-all
+                duration-150 focus:shadow-md focus:shadow-primary/15 focus:ring-2 focus:ring-primary',
+                'border-red-500' => $errors->has('name'),
+            ])
+            ->merge([
+                'type' => 'text'
+            ])
     }}
 />
---
+++ resources/views/components/forms/textarea.blade.php
@@ -0,0 +1,21 @@
+@aware([
+    'name',
+    'required',
+])
+
+<textarea
+    name="{{ $name }}"
+    id="{{ $name }}"
+    @if ($required) required @endif
+    {{
+        $attributes
+        ->class([
+            'w-full bg-white border border-gray-200 text-secondary rounded px-4 py-2 outline-none transition-all
+             duration-150 focus:shadow-md focus:shadow-primary/15 focus:ring-2 focus:ring-primary',
+            'border-red-500' => $errors->has($name),
+        ])
+        ->merge([
+            'rows' => '4'
+        ])
+    }}
+>{{ $value ?? '' }}</textarea>
