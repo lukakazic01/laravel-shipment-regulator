a8e32d4651c135d32cf4829e800653b5fcf86d9f
Luka <kazicluka4@gmail.com>
2026-07-27 20:40:59 +0200

    Defined mutator for status in case wrong status is sent

--- app/Models/Shipment.php
+++ app/Models/Shipment.php
@@ -6,6 +6,7 @@ use Database\Factories\ShipmentFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Attributes\UseFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -31,5 +32,11 @@ class Shipment extends Model
 
     public function user(): BelongsTo {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    protected function status(): Attribute {
+        return Attribute::make(
+            set: fn (string $value) => in_array($value, self::SHIPMENT_STATUSES, true) ? $value : self::STATUS_UNASSIGNED,
+        );
     }
 }
