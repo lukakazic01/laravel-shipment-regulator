efba8a7261cb3eb5b381b59177e45da9c72b24b0
lukakazic01 <kazicluka4@gmail.com>
2026-07-29 10:51:23 +0200

    Create profile controller

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\NewAvatarRequest;
+
+class ProfileController extends Controller
+{
+
+    public function index() {
+        return view('profile.index');
+    }
+
+    public function changeAvatar(NewAvatarRequest $request) {
+        $this->deleteImageFromStorage(auth()->user()->avatar, "images/avatars/");
+        $name = $this->uploadImage("profile_image", "images/avatars/");
+        auth()->user()->update(['avatar' => $name]);
+        return redirect()->back();
+    }
+
+}
