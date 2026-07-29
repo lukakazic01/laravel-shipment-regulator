e05c398e2ba4b03e63cc740ef759872a8dfe7769
Luka <kazicluka4@gmail.com>
2026-07-29 20:01:33 +0200

    Exercise 29 homework (Migration)

---
+++ database/migrations/2026_07_29_174201_update_status_default_and_make_user_id_nullable_on_shipments_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Shipment;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            $table->string('status')->default(Shipment::STATUS_UNASSIGNED)->change();
+            $table->unsignedBigInteger('user_id')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('shipments', function (Blueprint $table) {
+            $table->string('status')->default(Shipment::STATUS_IN_PROGRESS)->change();
+            $table->unsignedBigInteger('user_id')->nullable(false)->change();
+        });
+    }
+
+};
