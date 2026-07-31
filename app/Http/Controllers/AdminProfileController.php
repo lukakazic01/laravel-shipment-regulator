a12f5a2ab4f4941fd9d96efd62b05b29c1531793
Luka <kazicluka4@gmail.com>
2026-07-31 19:45:50 +0200

    Caching users on admin profile page

--- app/Http/Controllers/AdminProfileController.php
+++ app/Http/Controllers/AdminProfileController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 
 class AdminProfileController extends Controller
 {
 
     public function index()
     {
-        $users = User::all();
+        $users = User::query()->hydrate(Cache::remember('admin-users', 600, fn () => User::all()->toArray()));
         return view('admin.profile.index', compact('users'));
     }
 
