7f0b9e77d1fb8d590ce45bb1537d7d56a5e9713c
Luka <kazicluka4@gmail.com>
2026-08-16 20:10:43 +0200

    Decrement and increment with addend

---
+++ resources/views/components/⚡shipments-assigned-list.blade.php
@@ -0,0 +1,34 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    public int $count = 0;
+    public int $addend = 1;
+
+    public function increment() {
+        $this->count+=$this->addend;
+    }
+
+    public function decrement() {
+        if ($this->count > 0) {
+            $this->count-=$this->addend;
+        }
+    }
+
+};
+?>
+
+<div class="flex flex-col gap-2">
+    <button class="bg-blue-300 text-white p-1" type="button" wire:click="increment">
+        Increment {{$count}}
+    </button>
+
+
+    <button class="bg-red-300 text-white p-1" type="button" wire:click="decrement">
+        Decrement {{$count}}
+    </button>
+
+    <input class="border border-gray-200 rounded p-2" type="number" min="1" wire:model.live="addend" />
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,6 @@
     <p class="text-center text-sm">
         Welcome to our shipment manager, to see all the shipments,
         <a href="{{ route('shipments.index') }}" class="text-blue-500">go here</a>
+        <livewire:shipments-assigned-list />
     </p>
 </x-layout>
