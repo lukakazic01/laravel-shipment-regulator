85a45d78ac1c50c87574ce7e81ff25cd3415a5e0
lukakazic01 <kazicluka4@gmail.com>
2026-08-17 14:47:20 +0200

    Remove live from model

--- resources/views/components/⚡create-shipment.blade.php
+++ resources/views/components/⚡create-shipment.blade.php
@@ -48,53 +48,53 @@ new class extends Component {
     @csrf
     <x-forms.field name="title">
         <x-forms.label>Title</x-forms.label>
-        <x-forms.input wire:model.live.debounce="title" :value="old('title', '')"/>
+        <x-forms.input wire:model.debounce="title" :value="old('title', '')"/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-forms.field name="fromCity">
         <x-forms.label>From city</x-forms.label>
-        <x-forms.input wire:model.live.debounce="fromCity" :value="old('fromCity', '')"/>
+        <x-forms.input wire:model.debounce="fromCity" :value="old('fromCity', '')"/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-forms.field name="fromCountry">
         <x-forms.label>From country</x-forms.label>
-        <x-forms.input wire:model.live.debounce="fromCountry" :value="old('fromCountry', '')"/>
+        <x-forms.input wire:model.debounce="fromCountry" :value="old('fromCountry', '')"/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-forms.field name="toCity">
         <x-forms.label>To city</x-forms.label>
-        <x-forms.input wire:model.live.debounce="toCity" :value="old('toCity', '')"/>
+        <x-forms.input wire:model.debounce="toCity" :value="old('toCity', '')"/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-forms.field name="toCountry">
         <x-forms.label>To country</x-forms.label>
-        <x-forms.input wire:model.live.debounce="toCountry" :value="old('toCountry', '')"/>
+        <x-forms.input wire:model.debounce="toCountry" :value="old('toCountry', '')"/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-forms.field name="status">
         <x-forms.label>Status</x-forms.label>
-        <x-forms.select wire:model.live.debounce="status" :values="$shipmentStatuses"
+        <x-forms.select wire:model.debounce="status" :values="$shipmentStatuses"
                         :selected="Shipment::STATUS_UNASSIGNED"/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-forms.field name="clientId">
         <x-forms.label>Client</x-forms.label>
-        <x-forms.select wire:blur="validateUser" wire:model.live.debounce="clientId" :values="$users"/>
+        <x-forms.select wire:blur="validateUser" wire:model.debounce="clientId" :values="$users"/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-forms.field name="price">
         <x-forms.label>Price</x-forms.label>
-        <x-forms.input wire:model.live.debounce="price" type="number" :value="old('price', '')"/>
+        <x-forms.input wire:model.debounce="price" type="number" :value="old('price', '')"/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-forms.field name="details">
         <x-forms.label>Details</x-forms.label>
-        <x-forms.textarea wire:model.live.debounce="details" :value="old('details', '')"/>
+        <x-forms.textarea wire:model.debounce="details" :value="old('details', '')"/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-forms.field name="documents[]">
         <x-forms.label>Documents</x-forms.label>
-        <x-forms.file-upload wire:model.live.debounce="documents" multiple/>
+        <x-forms.file-upload wire:model.debounce="documents" multiple/>
         <x-forms.error-message/>
     </x-forms.field>
     <x-base-button type="submit">Create shipment</x-base-button>
