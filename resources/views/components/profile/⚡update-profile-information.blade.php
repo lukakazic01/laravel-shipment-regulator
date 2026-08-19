549ad52052d980018c658f20e0e500fd1f5f5d37
lukakazic01 <kazicluka4@gmail.com>
2026-08-19 13:30:25 +0200

    Update user profile information through livewire

--- resources/views/components/profile/⚡update-profile-information.blade.php
+++ resources/views/components/profile/⚡update-profile-information.blade.php
@@ -1,16 +1,30 @@
 <?php
 
+use App\Actions\Fortify\UpdateUserProfileInformation;
 use Livewire\Component;
 
-new class extends Component
-{
+new class extends Component {
     public string $name = "";
     public string $email = "";
 
-    public function mount() {
+    public function mount()
+    {
         $this->name = auth()->user()->name;
         $this->email = auth()->user()->email;
     }
+
+    public function updateProfileInformation(UpdateUserProfileInformation $updateProfileInformation)
+    {
+        $updateProfileInformation->update(
+            auth()->user(),
+            [
+                'name' => $this->name,
+                'email' => $this->email
+            ]
+        );
+        $this->dispatch('success', message: 'Successfully updated user profile information');
+    }
+
 };
 ?>
 
@@ -19,25 +33,26 @@ new class extends Component
         <h1 class="text-2xl font-bold text-gray-900">Profile Information</h1>
         <p class="text-sm text-gray-500 mt-1">Update your account's profile information and email address.</p>
     </div>
-    <form action="{{ route('user-profile-information.update') }}" method="POST" class="space-y-6">
+    <form wire:submit="updateProfileInformation" action="{{ route('user-profile-information.update') }}" method="POST"
+          class="space-y-6">
         @csrf
         @method('PUT')
         <x-forms.field :has-error="$errors->updateProfileInformation->has('name')" name="name" required>
             <x-forms.label>Name</x-forms.label>
-            <x-forms.input wire:model.debounce.300ms="name" />
+            <x-forms.input wire:model.debounce.300ms="name"/>
             <x-forms.error-message>
                 {{ $errors->updateProfileInformation->first('name') }}
             </x-forms.error-message>
         </x-forms.field>
         <x-forms.field :has-error="$errors->updateProfileInformation->has('email')" name="email" required>
             <x-forms.label>Email</x-forms.label>
-            <x-forms.input wire:model.debounce.300ms="email" />
+            <x-forms.input wire:model.debounce.300ms="email"/>
             <x-forms.error-message>
                 {{ $errors->updateProfileInformation->first('email') }}
             </x-forms.error-message>
         </x-forms.field>
         <div class="flex items-center justify-end gap-3 pt-2">
-            <x-base-button type="submit">Save</x-base-button>
+            <x-base-button loader-target="updateProfileInformation" type="submit">Save</x-base-button>
         </div>
     </form>
 </div>
