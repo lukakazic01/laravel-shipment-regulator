3d9fb766b7fb5c16dbcd139a2a792197d45894ac
Luka <kazicluka4@gmail.com>
2026-08-20 20:45:05 +0200

    One root element as well

--- resources/views/pages/shipments/⚡index.blade.php
+++ resources/views/pages/shipments/⚡index.blade.php
@@ -31,17 +31,19 @@ new class extends Component {
 ?>
 
 <x-slot:title>All shipments</x-slot:title>
-<x-base.session-message :alert-severity="AlertSeverity::Success"/>
-<div class="flex flex-col gap-6">
-    @forelse($shipments as $shipment)
-        <x-shipment-card :shipment="$shipment" :users="$users"/>
-    @empty
-        <p class="text-center text-secondary text-sm">
-            We currently don't have any unassigned shipment
-            @can('create', Shipment::class)
-                ,go ahead and
-                <a href="{{ route("shipments.create") }}" class="text-blue-500">create one</a>
-            @endcan
-        </p>
-    @endforelse
+<div>
+    <x-base.session-message :alert-severity="AlertSeverity::Success"/>
+    <div class="flex flex-col gap-6">
+        @forelse($shipments as $shipment)
+            <x-shipment-card :shipment="$shipment" :users="$users"/>
+        @empty
+            <p class="text-center text-secondary text-sm">
+                We currently don't have any unassigned shipment
+                @can('create', Shipment::class)
+                    ,go ahead and
+                    <a href="{{ route("shipments.create") }}" class="text-blue-500">create one</a>
+                @endcan
+            </p>
+        @endforelse
+    </div>
 </div>
